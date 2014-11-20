a4929bbd985db6c8a711a9321e7ee30043eb875e
srgg6701 <srgg67@gmail.com>
2014-11-20 20:51:06 +0300

    Создан прототип обработчика изменения группы юзера на клиенте.

--- components/com_sample/views/default/tmpl/default.php
+++ components/com_sample/views/default/tmpl/default.php
@@ -4,15 +4,34 @@ defined('_JEXEC') or die('Restricted access');
 	?><h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 <?php endif;
 ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <style>
+    table.common{
+        border: solid 2px #ccc;
+    }
+    div.close{
+        color: red;
+        cursor: pointer;
+        display: inline-block;
+        height: 16px;
+        margin: 0 -6px -3px 10px;
+        transform: scaleX(1.2);
+        width: 16px;
+    }
     .common th,
     .common td{
         padding: 2px 4px;
     }
+    .common tr td:last-child span{
+        color: navy;
+        cursor: pointer;
+    }
+    .groups_list{
+        margin: -2px -5px;
+    }
 </style>
-<?php var_dump($this->usergroups);
-?>
 <h2>Юзеры</h2>
+<p>Клацните по группе юзера, чтобы выбрать ей достойную замену.</p>
 <table class="common">
     <tr>
         <th>id</th>
@@ -29,9 +48,43 @@ defined('_JEXEC') or die('Restricted access');
         <td><?php echo $user->name;?></td>
         <td><?php echo $user->username;?></td>
         <td><?php echo $user->email;?></td>
-        <td><?php
-            echo str_replace("\n",", ",$user->group_names);?></td>
+        <td><span><?php
+            echo str_replace("\n","</span>, <span>",$user->group_names);
+                ?></span>
+        </td>
     </tr>
 <?php   endforeach;
 ?>
 </table>
+<button id="btn-apply">Подтвердить!</button>
+<script>
+$(function(){
+    var user_groups='<select class="groups_list">';
+    <?php
+    foreach($this->usergroups as $i=>$group_data):?>
+        user_groups+='<option value="<?php echo $group_data->id;?>">';
+        user_groups+='<?php echo $group_data->title;?>';
+        user_groups+='</option>';
+    <?php
+    endforeach; echo "\n";?>
+    user_groups+='</select><div class="close">x</div>';
+
+    // отобразить список групп для замены
+    $('td span').on('click', function(){
+        $(this).after(user_groups).hide();
+        var tSelect = $(this).next()[0],
+            opt = $('option:contains('+$(this).text()+')', tSelect);
+        tSelect.options[$(opt).index()].selected=true
+    });
+    // отменить выбор группы
+    $('.common').on('click', '.close',function(event){
+        $(event.currentTarget).prev().prev().show();
+        $(event.currentTarget).prev().remove();
+        $(event.currentTarget).remove();
+    });
+    // подтвердить смену группы юзера
+    $('#btn-apply').on('click', function(){
+
+    });
+});
+</script>
